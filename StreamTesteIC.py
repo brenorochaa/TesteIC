6cfae17ca22d8dedd0d135eeab06037dd8b92bff
brenorochaa <127889210+brenorochaa@users.noreply.github.com>
2023-03-15 19:05:31 -0300

    Update StreamTesteIC.py

--- StreamTesteIC.py
+++ StreamTesteIC.py
@@ -6,7 +6,7 @@ st.title("Engenharia de Manutenção - RANDOM UFPE")
 st.header("Políticas de manutenção por idade")
 st.subheader("Como encontrar os parâmetros Beta e Eta pra a distribuição Weibull")
 st.write("Encontrar os parâmetros Beta e Eta para se aplicar na distribuição de probabilidade Weibull não é uma tarefa fácil. Para isso, desenvolvemos esta ferramenta de modo que utiliza dados estatísticos a fim de simplificar o processo de obtenção desse resultado")
-if st.checkbox("Estou disposto a participar"):
+if st.checkbox("Estou disposto a participar","Digite aqui"):
     st.text("Muito bem! vamos nessa!")
 nome = st.text_input("Qual o seu nome?")
 status = st.selectbox("Qual sua atuação: ", ['Estudante', 'Professor', 'Outro'])
