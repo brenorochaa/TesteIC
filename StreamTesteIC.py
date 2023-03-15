241f49cf85c747d28e800e34d415d30d967fc3fa
brenorochaa <127889210+brenorochaa@users.noreply.github.com>
2023-03-15 14:49:21 -0300

    Add files via upload

---
+++ StreamTesteIC.py
@@ -0,0 +1,14 @@
+import streamlit as st
+#from PIL import Image
+#img = open('Users\breno\Downloads\random_logo')
+#st.image(img, caption = 'Random UFPE')
+st.title("Engenharia de Manutenção - RANDOM UFPE")
+st.header("Políticas de manutenção por idade")
+st.subheader("Como encontrar os parâmetros Beta e Eta pra a distribuição Weibull")
+st.write("Encontrar os parâmetros Beta e Eta para se aplicar na distribuição de probabilidade Weibull não é uma tarefa fácil. Para isso, desenvolvemos esta ferramenta de modo que utiliza dados estatísticos a fim de simplificar o processo de obtenção desse resultado")
+if st.checkbox("Estou disposto a participar"):
+    st.text("Muito bem! vamos nessa!")
+nome = st.text_input("Qual o seu nome?")
+st.write("nome registrado!")
+status = st.selectbox("Qual sua atuação: ", ['Estudante', 'Professor', 'Outro'])
+st.write('Sua opção escolhida foi: ',status)
